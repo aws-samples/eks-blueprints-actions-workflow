ee2fd1ca47e3b67a2f392d2ac79ff80c4e8b4ed2
micbegin <micbegin@amazon.com>
2022-12-02 09:49:47 -0500

    Debug destroy pr-temp cluster 1941

--- main.tf
+++ main.tf
@@ -171,7 +171,7 @@ module "eks_blueprints" {
 }
 
 module "eks_blueprints_kubernetes_addons" {
-  depends_on = [module.eks_blueprints.fargate_profiles]
+  # depends_on = [module.eks_blueprints.fargate_profiles]
 
   source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.17.0"
 
